1cf924ec89fceb6a3f1b6395a3202d97bce2ed25
Spectre Security <42466179+SpectreSecurity@users.noreply.github.com>
2025-01-18 23:41:47 -0500

    Update ubuntu-18.04-5.6.1-mn-install.sh

--- ubuntu-18.04-5.6.1-mn-install.sh
+++ ubuntu-18.04-5.6.1-mn-install.sh
@@ -164,7 +164,7 @@ clear
 function copy_gamefrag_binaries(){
   cd /root
   apt-get install wget unzip -qq
-  wget $GAMEFRAG_LATEST_RELEASE
+  wget -v $GAMEFRAG_LATEST_RELEASE
   unzip gamefrag-5.6.1-ubuntu18-daemon.zip
   cp gamefrag-cli gamefragd gamefrag-tx /usr/local/bin >/dev/null
   chmod 755 /usr/local/bin/gamefrag* >/dev/null
